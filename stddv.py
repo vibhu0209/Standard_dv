e764cafb95f0d05a7468fc567ee3513907361f5c
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2022-01-21 16:55:08 +0530

    Add files via upload

---
+++ stddv.py
@@ -0,0 +1,33 @@
+import math
+import csv
+
+with open("E:\WHITEHAT JR\python\P-105\data.csv", newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+data = file_data[0]
+
+def mean(data):
+    n= len(data)
+    total =0
+    for x in data:
+        total += int(x)
+
+    mean = total / n
+    return mean
+
+squared_list= []
+    
+for number in data:
+    a = int(number) - mean(data)
+    a= a**2
+    squared_list.append(a)
+
+sum = 0
+for i in squared_list:
+    sum += i
+
+result = sum/ (len(data)-1)
+
+std_deviation = math.sqrt(result)
+print(std_deviation)
